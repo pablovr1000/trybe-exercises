5cfedddca94423240c1057ca85a4c8832c408da6
Laura Lima <lauraolima2000@gmail.com>
2021-11-05 15:26:03 -0300

    add .

---
+++ fundamentos/bloco-04-introducao-js-e-logica-programacao/4.1_primeiros_passos/ex1.js
@@ -0,0 +1,11 @@
+const myName = "Laura";
+const birthCity = "Santos";
+var birthYear = "2000";
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
