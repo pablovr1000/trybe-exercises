01836f4c9d0055b6d1ee105012c1f2d4e2c376b8
Laura Lima <lauraolima2000@gmail.com>
2021-11-08 15:47:24 -0300

    add exercícios 4.2

---
+++ fundamentos/bloco-04-introducao-js-e-logica-programacao/dia 4.2  - array-e-loop-for/ex1.js
@@ -0,0 +1,7 @@
+// adiciona mais uma tarefa no início da lista.
+
+let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+tasksList.unshift('Fazer exercícios da Trybe');  
+console.log(tasksList);
+
