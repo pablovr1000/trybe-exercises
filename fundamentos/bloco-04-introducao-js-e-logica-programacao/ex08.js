24db7e3380175995b0d29d9b38a6fa08d92a4e2d
Laura Lima <93014175+Thisislauralima@users.noreply.github.com>
2021-11-08 15:55:52 -0300

    Delete ex08.js

--- fundamentos/bloco-04-introducao-js-e-logica-programacao/ex08.js
+++
@@ -1,8 +0,0 @@
-const a = 5;
-const b = 8;
-
-console.log('Soma: ' + (a + b));
-console.log('Subtração: ' + (a - b));
-console.log('Multiplição: ' + (a * b));
-console.log('Divisão: ' + (a / b));
-console.log('Módulo: ' + (a % b));
