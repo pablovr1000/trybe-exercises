54d6d1b0ce207824ecae1eae8ae203a2d573cfce
Laura Lima <lauraolima2000@gmail.com>
2021-12-25 20:47:57 -0300

    ex bonus dia 8.1

--- fundamentos/Bloco-08-Higher-order-Functions/dia_01_Introducao-a-higher-order-functions/ex-bonus.js
+++ fundamentos/Bloco-08-Higher-order-Functions/dia_01_Introducao-a-higher-order-functions/ex-bonus.js
@@ -1,7 +1,7 @@
 const mage = {
   healthPoints: 130,
   intelligence: 45,
-  mana: 125,
+  mana: 16,
   damage: undefined,
 };
 
@@ -24,14 +24,69 @@ const battleMembers = { mage, warrior, dragon };
 // O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
 function dragonDamage() {
   return dragon.damage = Math.round(Math.random() * (dragon.strength - 15) + 15);
-}
-console.log(dragonDamage());
+};
 
 // 2 - Crie uma função que retorna o dano causado pelo warrior.
 // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
 
-function test() {
+function warriorDamage() {
   return warrior.damage = Math.round(Math.random() * (warrior.strength * warrior.weaponDmg - warrior.strength) + warrior.strength);
-}
-console.log(test());
+};
 
+// 3 - Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+
+function damageAndManaSpentyByMage() {
+  const mageDamageFormula = Math.round((Math.random() * (mage.intelligence * 2 - mage.intelligence)) + mage.intelligence);
+  const mageStatus = {
+  manaPerRound: 0,
+  mageDamagePerRound: 'Não possui mana o suficiente',
+  };
+
+  if (mage.mana > 15) {
+    mageStatus.manaPerRound = 15;
+    mageStatus.mageDamagePerRound = mageDamageFormula;
+  }
+
+  return mageStatus;
+};
+
+// Agora que você já possui a implementação das funções relativas aos três exercícios anteriores, passe-as como parâmetro para outras funções que irão compor um objeto gameActions . O objeto será composto por ações do jogo e cada ação é por denifição uma HOF , pois neste caso, são funções que recebem como parâmetro outra função.
+
+// 1 - Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior .
+
+
+// 2 - Crie a segunda HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem mage . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem mage e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor das chaves damage e mana do mage.
+
+// 3 - Crie a terceira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do monstro dragon . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo monstro dragon e atualizará os healthPoints dos personagens mage e warrior . Além disto ela também deve atualizar o valor da chave damage do monstro.
+
+// 4 - Adicione ao objeto gameActions uma função que retorne o objeto battleMembers atualizado e faça um console.log para visualizar o resultado final do turno.
+
+const gameActions = {
+  warriorRound: (warriorDamage) => {
+    const warriorAtack = warriorDamage();
+    warrior.damage = warriorAtack;
+    dragon.healthPoints -= warriorAtack;
+  },
+  mageRound: (damageAndManaSpentyByMage) => {
+    const mageAttack = damageAndManaSpentyByMage(); // manaPerRound e mageDamagePerRound
+    mage.damage = mageAttack.mageDamagePerRound;
+    mage.mana -= mageAttack.manaPerRound;
+    if (typeof mageAttack.mageDamagePerRound === 'number') {
+      dragon.healthPoints -= mageAttack.mageDamagePerRound;
+    }
+  },
+  dragonRound: (dragonDamage) => {
+    const dragonAttack = dragonDamage(); // eu tenho que criar uma variável para que sempre seja criado um valor diferente
+    dragon.damage = dragonAttack;
+    mage.healthPoints -= dragonAttack;
+    warrior.healthPoints -= dragonAttack;
+  },
+  battleMembersFinalResult: () => battleMembers,
+};
+gameActions.warriorRound(warriorDamage);
+gameActions.mageRound(damageAndManaSpentyByMage);
+gameActions.dragonRound(dragonDamage);
+
+console.log(gameActions.battleMembersFinalResult());
