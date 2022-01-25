a5c764c655f839af9150f0441ccd6f41c57bd2c9
Laura Lima <lauraolima2000@gmail.com>
2022-01-25 15:18:32 -0300

    exs dia1

---
+++ front-end/bloco-01-introducao-a-react/dia-01-introducao-a-react/ex2/src/App.js
@@ -0,0 +1,16 @@
+import Header from './Header.jsx';
+import Content from './Content.jsx';
+import Footer from './Footer.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Content />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
